2860b21d4dbb72bc1072a99dca929de0b5ee7375
yeedme <73169342+yeedme@users.noreply.github.com>
2022-10-01 11:39:25 +0800

    add

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-import ContentTop from '../../compontent/ContentTop/ContentTop'
+import React from "react";
+import ContentTop from "../../compontent/ContentTop/ContentTop";
+import './Overview.css'
 export default function Overview() {
   return (
-    <div><ContentTop/></div>
-  )
+    <div className="OverView-Content">
+      <div>
+        <ContentTop />
+      </div>
+      <div className=" DataCardList"> 
+        <div className="DataCard"/>
+        <div className="DataCard"/>
+        <div className="DataCard"/>
+        <div className="DataCard"/>
+      </div>
+    </div>
+  );
 }
