bd1bb903c3a7d50a0d88d64b5fb335a840bdba82
yeedme <73169342+yeedme@users.noreply.github.com>
2022-10-09 15:00:06 +0800

    add

--- src/pages/order/AddOrder.jsx
+++ src/pages/order/AddOrder.jsx
@@ -1,44 +1,130 @@
 import React, { useState } from "react";
-import { Avatar, Input ,Segmented} from "antd";
+import { Avatar, Input, Segmented, InputNumber, DatePicker } from "antd";
 
-import { UserOutlined, PhoneOutlined,CommentOutlined } from "@ant-design/icons";
-import  OrderCard from"../../compontent/order/OrderCard"
+import {
+  UserOutlined,
+  PhoneOutlined,
+  CommentOutlined,
+} from "@ant-design/icons";
+import OrderCard from "../../compontent/order/OrderCard";
 import "./AddOrder.css";
-const defaultData={
-  PaymentStatus:'paid',
-  name:'joye',
-  AvatarImg:'defaultData',
-  OrderID:'defaultData',
-  Data:'defaultData',
-  Price:'defaultData',
-  comment:'defaultData',
-  phone:'defaultData',
-  email:'defaultData'
-}
+//默认数据便于展示
+const defaultData = {
+  PaymentStatus: "paid",
+  name: "joye",
+  AvatarImg: "defaultData",
+  OrderID: "defaultData",
+  Date: "defaultData",
+  Price: "defaultData",
+  comment: "defaultData",
+  phone: "defaultData",
+  email: "defaultData",
+};
 export default function AddOrder() {
-  const [OrderData,setOrderData]=useState(defaultData)
-  const k=(e,type)=>{
-    let value=e.target.value;
-    let name=value;
-    setOrderData({...OrderData,name})
-   }
+  const [OrderData, setOrderData] = useState(defaultData);
   return (
-    <div>
+  <div>
+    <h2>AddOrder</h2>
+    <p>Here can push your information  .
+      And review Card</p>
+      <div className="AddOrder">
+ {/* ------------------预览 卡片信息 ------- */}
       <div className="AddOrder-Avatar">
-      <OrderCard OrderCardDate={OrderData}/>
+        <OrderCard OrderCardDate={OrderData} />
       </div>
 
       <div className="AddOrder-Info">
+
+
+  {/* ----------------------------OrderId-------------- */}
+        OrderId:
+        <Input
+          prefix={<UserOutlined />}
+          size="large"
+          onChange={(e) => {
+            setOrderData({ ...OrderData, OrderID: e.target.value });
+          }}
+        />
+
+    {/* ----------------------------OrderId-------------- */}
         name:
-        <Input prefix={<UserOutlined />} onChange	={(e)=>k(e,'name')} />
+        <Input
+          prefix={<UserOutlined />}
+          size="large"
+          onChange={(e) => {
+            setOrderData({ ...OrderData, name: e.target.value });
+          }}
+        />
+
+
+   {/* ----------------------------phone-------------- */}
         phone:
-        <Input prefix={<PhoneOutlined />} />
+        <Input
+          prefix={<PhoneOutlined />}
+          size="large"
+          onChange={(e) => {
+            setOrderData({ ...OrderData, phone: e.target.value });
+          }}
+        />
+
+
+   {/* ----------------------------email-----------0---------- */}
         email:
-        <Input prefix={<CommentOutlined />} />
+        <Input
+          prefix={<CommentOutlined />}
+          size="large"
+          onChange={(e) => {
+            setOrderData({ ...OrderData, email: e.target.value });
+          }}
+        />
+
+
+  {/* -------------PaymentStatus-------------------- */}
         PaymentStatus:
-        <Segmented block options={['paid', 'pending', 'cancelled']} />
+        <Segmented
+          size="large"
+          block
+          options={["paid", "pending", "cancelled"]}
+          onChange={(value) => {
+            setOrderData({ ...OrderData, PaymentStatus: value });
+          }}
+        />
+
+
+      {/* ----------------------------price--------------------- */}
+            price:
+        <div className="AddOrder-options">  
+          <br />
+          <InputNumber
+            min={0}
+            defaultValue={10}
+            onChange={(value) => {
+              setOrderData({ ...OrderData, Price: value });
+            }}
+            className="AddOrder-compontent"
+          />       
+        </div>
+
+
+    {/* ----------------------------Date------------------------- */}
+        <div className="AddOrder-options">
+          Date : <br />
+          <DatePicker
+            onChange={(date, dateString) => {
+              setOrderData({ ...OrderData, Date: dateString });
+            }}
+            className="AddOrder-compontent"
+          />
+        </div>
+
+    {/* ----------------------------Comment------------------------- */}
+        <div className="AddOrder-options">
+          Comment :<br />
+          <textarea className="AddOrder-TextArea"></textarea>
+        </div>
 
       </div>
-    </div>
+      </div>
+    </div>    
   );
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,60 @@
-
-import  Transit  from "../pages/Transit";
+import Transit from "../pages/Transit";
 import React from "react";
 //路由懒加载
-const Home=React.lazy(()=>import('../compontent/home/Home'));
-const Login=React.lazy(()=>import('../Login/Login'));
-const OverView=React.lazy(()=>import('../pages/overview/Overview'));
-const Order=React.lazy(()=>import('../pages/order/Order'));
-const Products=React.lazy(()=>import('../pages/Products/Products'));
-const AddProducts=React.lazy(()=>import('../pages/Products/AddProducts'));
-const AllProducts=React.lazy(()=>import('../pages/Products/AllProducts'));
-const routers=[{
-    path:"/transit",
-    element:<Transit/>,
-    children:[{
-        path:"home",
-        element:<Home/>,
-        children:[{
-            path:"overview",
-            element:<OverView/>,
-        },{
-            path:"order",
-            element:<Order/>,
-        },{
-            path:"products",
-            element:<Products/>,children:[{
-                path:"allproducts",
-                element:<AllProducts/>
-            },{
-                path:"addproducts",
-                element:<AddProducts/>
-            }]
-        }]
-    }]
-},
-{
-    path:'/login',
-    element:<Login/>
-},{
-    path:'/',
-    element:<Login/>
-}]
-export default routers
+const Home = React.lazy(() => import("../compontent/home/Home"));
+const Login = React.lazy(() => import("../Login/Login"));
+const OverView = React.lazy(() => import("../pages/overview/Overview"));
+const AllOrder = React.lazy(() => import("../pages/order/AllOrder"));
+const AddOrder = React.lazy(() => import("../pages/order/AddOrder"));
+const Products = React.lazy(() => import("../pages/Products/Products"));
+const AddProducts = React.lazy(() => import("../pages/Products/AddProducts"));
+const AllProducts = React.lazy(() => import("../pages/Products/AllProducts"));
+const routers = [
+  {
+    path: "/transit",
+    element: <Transit />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+        children: [
+          {
+            path: "overview",
+            element: <OverView />,
+          },
+          {
+            path: "order/allOrder",
+            element: <AllOrder />,
+          },
+          {
+            path: "order/addOrder",
+            element: <AddOrder />,
+          },
+          {
+            path: "products",
+            element: <Products />,
+            children: [
+              {
+                path: "allproducts",
+                element: <AllProducts />,
+              },
+              {
+                path: "addproducts",
+                element: <AddProducts />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <Login />,
+  },
+];
+export default routers;
