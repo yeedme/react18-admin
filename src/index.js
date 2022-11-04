d02c91da57280bf9301b2d5a2232d7089fd35819
yeedme <73169342+yeedme@users.noreply.github.com>
2022-11-04 14:15:12 +0800

    f

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import  { BrowserRouter} from 'react-router-dom'
+import  {HashRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+
+     <HashRouter>
+     <App />
+     </HashRouter>
  
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import LoginOrRegistered from './pages/LoginOrRegistered/LoginOrRegistered';
+
+// ReactDOM.render(
+//   <LoginOrRegistered />,
+//   document.getElementById('root')
+// );
 
